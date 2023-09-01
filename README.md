4e84d1965cd7a73d8784095a582436c3e6b14186
Markus Mayer <widemeadows@gmail.com>
2023-09-01 09:55:30 +0200

    Improve documentation

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-# timelag - creating time lagged data series
+# timelag — creating time lagged data series
 
 This crate provides `lag_matrix` and related functions to create time-lagged
 versions of time series.
+
+
+```rust
+use timelag::lag_matrix;
+
+fn it_works() {
+    let data = [1.0, 2.0, 3.0, 4.0];
+    
+    // Using infinity for padding because NaN doesn't equal itself.
+    let lag = f64::INFINITY;
+    let padding = f64::INFINITY;
+    
+    let direct = lag_matrix(&data, 3, lag, 5).unwrap();
+    
+    assert_eq!(
+        direct,
+        &[
+            1.0, 2.0, 3.0, 4.0, padding, // original data
+            lag, 1.0, 2.0, 3.0, padding, // first lag
+            lag, lag, 1.0, 2.0, padding, // second lag
+            lag, lag, lag, 1.0, padding, // third lag
+        ]
+    );
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! timelag — creating time lagged data series
+//!
+//! Functions for creating time-lagged versions of time series data.
+//!
+//! ## Example
+//! ```
+//! # use timelag::lag_matrix;
+//! let data = [1.0, 2.0, 3.0, 4.0];
+//!
+//! // Using infinity for padding because NaN doesn't equal itself.
+//! let lag = f64::INFINITY;
+//! let padding = f64::INFINITY;
+//!
+//! let direct = lag_matrix(&data, 3, lag, 5).unwrap();
+//!
+//! assert_eq!(
+//!     direct,
+//!     &[
+//!         1.0, 2.0, 3.0, 4.0, padding, // original data
+//!         lag, 1.0, 2.0, 3.0, padding, // first lag
+//!         lag, lag, 1.0, 2.0, padding, // second lag
+//!         lag, lag, lag, 1.0, padding, // third lag
+//!     ]
+//! );
+//! ```
+
 // SPDX-FileCopyrightText: 2023 Markus Mayer
 // SPDX-License-Identifier: EUPL-1.2
 
@@ -8,8 +34,56 @@
 use std::borrow::Borrow;
 use std::fmt::{Display, Formatter};
 
+/// The prelude.
+pub mod prelude {
+    pub use crate::CreateLagMatrix;
+}
+
 pub trait CreateLagMatrix<T> {
-    fn lag_matrix(&self, lags: usize, fill: T) -> Result<Vec<T>, LagError>;
+    /// Create a time-lagged matrix of time series values.
+    ///
+    /// This function creates lagged copies of the provided data and pads them with a placeholder value.
+    /// The source data is interpreted as increasing time steps with every subsequent element; as a
+    /// result, earlier (lower index) elements of the source array will be retained while later (higher
+    /// index) elements will be dropped with each lag. Lagged versions are prepended with the placeholder.
+    ///
+    /// ## Arguments
+    /// * `lags` - The number of lagged versions to create.
+    /// * `fill` - The value to use to fill in lagged gaps.
+    /// * `stride` - The number of elements between lagged versions in the resulting vector.
+    ///            If set to `0` or `data.len()`, no padding is introduced. Values larger than
+    ///            `data.len()` creates padding entries set to the `fill` value.
+    ///
+    /// ## Returns
+    /// A vector containing lagged copies of the original data, or an error.
+    ///
+    /// For `N` datapoints and `M` lags, the result can be interpreted as an `M×N` matrix with
+    /// lagged versions along the rows. With strides `S >= N`, the resulting matrix is of shape `M×S`
+    /// with an `M×N` submatrix at `0×0` and padding to the right.
+    ///
+    /// ## Example
+    /// ```
+    /// use timelag::prelude::*;
+    ///
+    /// let data = [1.0, 2.0, 3.0, 4.0];
+    ///
+    /// // Using infinity for padding because NaN doesn't equal itself.
+    /// let lag = f64::INFINITY;
+    /// let padding = f64::INFINITY;
+    ///
+    /// let direct = data.lag_matrix(3, lag, 5).unwrap();
+    ///
+    /// assert_eq!(
+    ///     direct,
+    ///     &[
+    ///         1.0, 2.0, 3.0, 4.0, padding, // original data
+    ///         lag, 1.0, 2.0, 3.0, padding, // first lag
+    ///         lag, lag, 1.0, 2.0, padding, // second lag
+    ///         lag, lag, lag, 1.0, padding, // third lag
+    ///     ]
+    /// );
+    /// ```
+    fn lag_matrix(&self, lags: usize, fill: T, stride: usize) -> Result<Vec<T>, LagError>;
 }
 
 impl<S, T> CreateLagMatrix<T> for S
@@ -17,12 +91,60 @@ where
     S: Borrow<[T]>,
     T: Copy,
 {
-    fn lag_matrix(&self, lags: usize, fill: T) -> Result<Vec<T>, LagError> {
-        lag_matrix(self.borrow(), lags, fill)
+    fn lag_matrix(&self, lags: usize, fill: T, stride: usize) -> Result<Vec<T>, LagError> {
+        lag_matrix(self.borrow(), lags, fill, stride)
     }
 }
 
-pub fn lag_matrix<T: Copy>(data: &[T], lags: usize, fill: T) -> Result<Vec<T>, LagError> {
+/// Create a time-lagged matrix of time series values.
+///
+/// This function creates lagged copies of the provided data and pads them with a placeholder value.
+/// The source data is interpreted as increasing time steps with every subsequent element; as a
+/// result, earlier (lower index) elements of the source array will be retained while later (higher
+/// index) elements will be dropped with each lag. Lagged versions are prepended with the placeholder.
+///
+/// ## Arguments
+/// * `data` - The time series data to create lagged versions of.
+/// * `lags` - The number of lagged versions to create.
+/// * `fill` - The value to use to fill in lagged gaps.
+/// * `stride` - The number of elements between lagged versions in the resulting vector.
+///            If set to `0` or `data.len()`, no padding is introduced. Values larger than
+///            `data.len()` creates padding entries set to the `fill` value.
+///
+/// ## Returns
+/// A vector containing lagged copies of the original data, or an error.
+///
+/// For `N` datapoints and `M` lags, the result can be interpreted as an `M×N` matrix with
+/// lagged versions along the rows. With strides `S >= N`, the resulting matrix is of shape `M×S`
+/// with an `M×N` submatrix at `0×0` and padding to the right.
+///
+/// ## Example
+/// ```
+/// # use timelag::lag_matrix;
+/// let data = [1.0, 2.0, 3.0, 4.0];
+///
+/// // Using infinity for padding because NaN doesn't equal itself.
+/// let lag = f64::INFINITY;
+/// let padding = f64::INFINITY;
+///
+/// let direct = lag_matrix(&data, 3, lag, 5).unwrap();
+///
+/// assert_eq!(
+///     direct,
+///     &[
+///         1.0, 2.0, 3.0, 4.0, padding, // original data
+///         lag, 1.0, 2.0, 3.0, padding, // first lag
+///         lag, lag, 1.0, 2.0, padding, // second lag
+///         lag, lag, lag, 1.0, padding, // third lag
+///     ]
+/// );
+/// ```
+pub fn lag_matrix<T: Copy>(
+    data: &[T],
+    lags: usize,
+    fill: T,
+    mut stride: usize,
+) -> Result<Vec<T>, LagError> {
     if lags == 0 || data.is_empty() {
         return Ok(data.to_vec());
     }
@@ -32,11 +154,19 @@ pub fn lag_matrix<T: Copy>(data: &[T], lags: usize, fill: T) -> Result<Vec<T>, L
         return Err(LagError::LagExceedsValueCount);
     }
 
-    let mut lagged = vec![fill; data_rows * (lags + 1)];
+    if stride == 0 {
+        stride = data_rows;
+    }
+
+    if stride < data_rows {
+        return Err(LagError::InvalidStride);
+    }
+
+    let mut lagged = vec![fill; stride * (lags + 1)];
     lagged[..data.len()].copy_from_slice(data);
 
     for lag in 1..=lags {
-        let lagged_offset = lag * data_rows + lag;
+        let lagged_offset = lag * stride + lag;
         let lagged_rows = data_rows - lag;
         let lagged_end = lagged_offset + lagged_rows;
         lagged[lagged_offset..lagged_end].copy_from_slice(&data[0..lagged_rows]);
@@ -48,6 +178,7 @@ pub fn lag_matrix<T: Copy>(data: &[T], lags: usize, fill: T) -> Result<Vec<T>, L
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum LagError {
     LagExceedsValueCount,
+    InvalidStride,
 }
 
 impl std::error::Error for LagError {}
@@ -61,6 +192,12 @@ impl Display for LagError {
                     "The specified lag exceeds the number of values in the time series"
                 )
             }
+            LagError::InvalidStride => {
+                write!(
+                    f,
+                    "The specified stride value must be greater than or equal to the number of elements in the data"
+                )
+            }
         }
     }
 }
@@ -70,11 +207,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_lag() {
         let data = [42.0, 40.0, 38.0, 36.0];
 
-        let direct = lag_matrix(&data, 3, f64::INFINITY).unwrap();
-        let implicit = data.lag_matrix(3, f64::INFINITY).unwrap();
+        let direct = lag_matrix(&data, 3, f64::INFINITY, 0).unwrap();
+        let implicit = data.lag_matrix(3, f64::INFINITY, 0).unwrap();
 
         assert_eq!(
             direct,
@@ -102,5 +239,91 @@ mod tests {
             ]
         );
         assert_eq!(direct, implicit);
+    }
+
+    #[test]
+    fn test_strided_lag_1() {
+        let data = [42.0, 40.0, 38.0, 36.0];
+
+        let direct = lag_matrix(&data, 3, f64::INFINITY, 5).unwrap();
+
+        assert_eq!(
+            direct,
+            &[
+                // original data
+                42.0,
+                40.0,
+                38.0,
+                36.0,
+                f64::INFINITY, // padding from stride
+                // first lag
+                f64::INFINITY, // lag
+                42.0,
+                40.0,
+                38.0,
+                f64::INFINITY, // padding from stride
+                // second lag
+                f64::INFINITY, // lag
+                f64::INFINITY, // lag
+                42.0,
+                40.0,
+                f64::INFINITY, // padding from stride
+                // third lag
+                f64::INFINITY, // lag
+                f64::INFINITY, // lag
+                f64::INFINITY, // lag
+                42.0,
+                f64::INFINITY, // padding from stride
+            ]
+        );
+    }
+
+    #[test]
+    fn test_strided_lag_2() {
+        let data = [42.0, 40.0, 38.0, 36.0];
+
+        let direct = lag_matrix(&data, 3, f64::INFINITY, 8).unwrap();
+
+        assert_eq!(
+            direct,
+            &[
+                // original data
+                42.0,
+                40.0,
+                38.0,
+                36.0,
+                f64::INFINITY, // padding from stride (1st)
+                f64::INFINITY, // padding from stride (2nd)
+                f64::INFINITY, // padding from stride (3rd)
+                f64::INFINITY, // padding from stride (4th)
+                // first lag
+                f64::INFINITY, // lag
+                42.0,
+                40.0,
+                38.0,
+                f64::INFINITY, // padding from stride (1st)
+                f64::INFINITY, // padding from stride (2nd)
+                f64::INFINITY, // padding from stride (3rd)
+                f64::INFINITY, // padding from stride (4th)
+                // second lag
+                f64::INFINITY, // lag
+                f64::INFINITY, // lag
+                42.0,
+                40.0,
+                f64::INFINITY, // padding from stride (1st)
+                f64::INFINITY, // padding from stride (2nd)
+                f64::INFINITY, // padding from stride (3rd)
+                f64::INFINITY, // padding from stride (4th)
+                // third lag
+                f64::INFINITY, // lag
+                f64::INFINITY, // lag
+                f64::INFINITY, // lag
+                42.0,
+                f64::INFINITY, // padding from stride (1st)
+                f64::INFINITY, // padding from stride (2nd)
+                f64::INFINITY, // padding from stride (3rd)
+                f64::INFINITY, // padding from stride (4th)
+            ]
+        );
     }
 }
